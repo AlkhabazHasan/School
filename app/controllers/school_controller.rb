799643860bf4205415ee411f8c3811b40a6a636f
Alkhabaz <Hasan.alkhabaz@uski.co>
2017-05-24 09:32:25 +0300

    school App

---
+++ app/controllers/school_controller.rb
@@ -0,0 +1,6 @@
+class SchoolController < ApplicationController
+
+	def home
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+School::Application.config.secret_token = '2c9081c93dad45120bff762b71914aaf280e3037d1c2ff763ffea615f5991d08791ba748ac10c36bcd5aff5fa545038bff67f0db55de36de760a5ec64f49f12b'
